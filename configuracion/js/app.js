56a93a032c57ea02ae399df71ecb2a82e32a97a4
Miguel-Rayon <miguel.hdz.rayon@gmail.com>
2022-05-23 00:09:21 -0500

    Mejora del Index

---
+++ configuracion/js/app.js
@@ -0,0 +1,53 @@
+/**
+ * Animacion del scroll para que se vea chingon xd
+ */
+window.onscroll = function(){
+    scroll = document.documentElement.scrollTop;
+    var posicion = window.pageYOffset || document.documentElement.scrollTop;
+
+    var elemento1 = document.getElementById("icon");
+    var elemento2 = document.getElementById("icon_dollar");
+
+    elemento1.style.bottom = posicion * 0.1 + "px";
+    elemento2.style.top = posicion * 0.1 + "px";
+
+    header = document.getElementById("header");
+
+    if (scroll > 20){
+        header.classList.add('nav_mod');
+    }else if(scroll < 20){
+        header.classList.remove('nav_mod');
+    }
+}
+/**
+ * Fin de la animacion del scroll
+ */
+/**
+ * Mover la barra de navegacion
+ */
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+
+menu = document.getElementById("header");
+body = document.getElementById("container_all");
+nav = document.getElementById("nav");
+
+    function mostrar_menu(){
+        
+        menu.classList.toggle('move_content');
+        body.classList.toggle('move_content');
+        nav.classList.toggle('move_nav');
+    }
+
+
+window.addEventListener("resize", function(){
+
+    if(window.innerWidth > 760){
+    menu.classList.remove('move_content');
+    body.classList.remove('move_content');
+    nav.classList.remove('move_nav');
+    }
+
+})
+/**
+ * Fin de mover la barra de navegacion 
+ */
--- vista/index.php
+++ vista/index.php
@@ -4,269 +4,146 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://kit.fontawesome.com/333b9b8f44.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../configuracion/css/estilos.css">
-    <style>
-    /* posicion del texto del navbar */
-    .navbar-nav {
-    width: 100%;
-    text-align: center;
-        }
-
-    .navbar-nav > li {
-        float: none;
-        display: inline-block;
-        }
-    /* hacer a las imagenes del carrusel responsivas */
-    .carousel-inner img {
-        width: 100%;
-        height: 100%;
-    }
-    </style>
-    <title>SUS BANK</title>
+    <title>SUS Bank</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-    <!-- logo -->
-    <a class="navbar-brand" href="#">
-        <img src="../img/logo.png" alt="logo" style="width:50px;">
-    </a>
-     <!-- Navbar text-->
-    <span class="navbar-text">
-    <dt>SUS Bank</dt>    
-    </span>
-    <span class="navbar-text">
-    ㅤㅤ    
-    </span>
-    <span class="navbar-text">
-    ㅤㅤ    
-    </span>
+    <!-- Barra de navegacion -->
+    <header id="header">
+        <div class="container_header">
+            <div class="logo">
+                <img src="../img/logo.png" alt="logo">
+            </div>
+            <div class="container__nav">
+                <nav id="nav">
+                    <ul>
+                        <li><a href="#" class="select">Inicio</a></li>
+                        <li><a href="#">Acceso</a></li>
+                        <li><a href="#">Sobre Nosotros</a></li>
+                        <li><a href="#">Ayuda</a></li>
+                    </ul>
+                </nav>
+                <div class="btn__menu" id="btn_menu"><i class="fas fa-bars"></i></div>
+            </div>
+        </div>
+    </header>
+    <!-- Fin Barra de navegacion -->
+    <!-- Portada -->
+    <div class="container__all" id="container_all">
+        <div class="cover">
+            <div class="container__cover">
+                <div class="container__info">
+                    <h1>SUS</h1>
+                    <H2>Bank</H2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, nam fuga delectus 
+                    laudantium tenetur eligendi magnam neque ullam? Id laborum, in dolore debitis nisi delectus 
+                    ipsum voluptate quae est libero?</p>
+                    <input type="button" value="Get Started">
+                </div>
+                <div class="container__vector">
+                    <img src="../img/Wallet_Monochromatic.svg" alt="portada" type="img/svg">
+                </div>
+            </div>
+        </div>
+    <!-- Fin Portada -->
+    <!-- Box de cards -->
+    <main>
+        <h1>Contratación digital</h1>
+        <div class="container__box">
+            <div class="box">
+                <i class="fa-solid fa-shop"></i>
+                <h5>Crédito de negocios</h5>
+                <h4>Crédito de negocios</h4>
+            </div>
+            <div class="box">
+                <i class="fa-solid fa-address-card"></i>
+                <h5>Cuenta</h5>
+                <h4>Cuenta</h4>
+            </div>
+            <div class="box">
+                <i class="fa-solid fa-wallet"></i>
+                <h5>Tarjeta de Credito</h5>
+                <h4>Tarjeta de Credito</h4>
+            </div>
+            <div class="box">
+                <i class="fa-solid fa-chart-pie"></i>
+                <h5>Prestamos</h5>
+                <h4>Prestamos</h4>
+            </div>
+            <div class="box">
+                <i class="fa-solid fa-chart-line"></i>
+                <h5>Inversiones</h5>
+                <h4>Inversiones</h4>
+            </div>
+            <div class="box">
+                <i class="fa-solid fa-stethoscope"></i>
+                <h5>Seguro de Vida</h5>
+                <h4>Seguro de Vida</h4>
+            </div>
+        </div><br>
+        <!-- Fin Box de cards -->
+        <!-- Banner -->
+        <div class="container__banner">
+            <div class="banner">
+                <div class="icon">
+                    <img src="../img/8712738_lock_iso_premium_icon.png"  id="icon" alt="">
+                </div>
+                <div class="img__icon_dollar">
+                    <img src="../img/8712511_dollar_money_currency_coin_payment_icon.png" id="icon_dollar" alt="">
+                </div>
+                <div class="text">
+                    <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident eaque et id veritatis aliquam nesciunt quasi..</h2>
+                    <input type="button" value="Leer más">
+                </div>
+            </div>
+        </div>
+        <!-- Fin Banner -->
+    </main>
     
-    <!-- Links -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav">
-       
-        <li class="nav-item">
-            <!-- dirige a la pagina:  -->
-            <a class="nav-link" href="#">Personas</a>
-        </li>
-        <span class="navbar-text">
-        ㅤㅤ    
-        </span>
-        <li class="nav-item">
-            <!-- dirige a la pagina:  -->
-            <a class="nav-link" href="#">Empresas</a>
-        </li>     
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            <span class="navbar-text">
-            ㅤㅤ    
-            </span>
-            <li class="nav-item"><a></a>ㅤㅤ</li>
-            <li class="nav-item"><a></a>ㅤㅤ</li>
-            <li class="nav-item"><a></a>ㅤㅤ</li>
-            <li class="nav-item"><a></a>ㅤㅤ</li>
-            <li class="nav-item"><i class="fa-solid fa-user" ></i><a href="#" class="nav-link"><small>Hazte cliente</small></a></li>
-            <li class="nav-item"><i class="fa-solid fa-user-check"></i><a href="#" class="nav-link"><small>Acceso</small> </a></li>
-            <li class="nav-item"><a></a>ㅤㅤ</li>
-            <li class="nav-item"><i class="fa-solid fa-question"></i><a class="nav-link" href="#"><small>Sobre Nosotros</small></a></li>
-        </ul>
-    </div>  
-    </nav>
-    <!-- Carrusel de imagenes -->
-    <div id="demo" class="carousel slide" data-ride="carousel">
-    <ul class="carousel-indicators">
-        <li data-target="#demo" data-slide-to="0" class="active"></li>
-        <li data-target="#demo" data-slide-to="1"></li>
-        <li data-target="#demo" data-slide-to="2"></li>
-    </ul>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-        <img src="../img/Vimprueb1.jpeg" alt="Img1" class="img-fluid">
-        <div class="carousel-caption">
-            <h3>Lorem Ipsum</h3>
-            <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
-        </div>   
-        </div>
-        <div class="carousel-item">
-        <img src="../img/Vimprueb2.jpeg" alt="Img2" class="img-fluid">
-        <div class="carousel-caption">
-            <h3>Lorem Ipsum</h3>
-            <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
-        </div>   
-        </div>
-        <div class="carousel-item">
-        <img src="../img/Vimprueb3.jpeg" alt="Img3" class="img-fluid">
-        <div class="carousel-caption">
-            <h3>Lorem Ipsum</h3>
-            <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
-        </div>   
-        </div>
-    </div>
-    <a class="carousel-control-prev" href="#demo" data-slide="prev">
-        <span class="carousel-control-prev-icon"></span>
-    </a>
-    <a class="carousel-control-next" href="#demo" data-slide="next">
-        <span class="carousel-control-next-icon"></span>
-    </a>
-    </div>
-    <br>
 
-    <div class="contenedor_grid">
-		<div class="contenedor_xd">
-			<div class="cardd" style="background-image: url('../img/card_prueba.jpg')">
-				<div class="textos">
-					<h3>Lorem ipsum</h3>
-					<button class="boton">Saber Más</button>
-				</div>
-			</div>
-			<div class="cardd" style="background-image: url('../img/card_prueba.jpg')">
-				<div class="textos">
-					<h3>Lorem ipsum</h3>
-					<button class="boton">Saber Más</button>
-				</div>
-			</div>
-			<div class="cardd" style="background-image: url('../img/card_prueba.jpg')">
-				<div class="textos">
-					<h3>Lorem ipsum</h3>
-					<button class="boton">Saber Más</button>
-				</div>
-			</div>
 
-			<div class="cardd" style="background-image: url('../img/card_prueba.jpg')">
-				<div class="textos">
-					<h3>Lorem ipsum</h3>
-					<button class="boton">Saber Más</button>
-				</div>
-			</div>
-			<div class="cardd" style="background-image: url('../img/card_prueba.jpg')">
-				<div class="textos">
-					<h3>Lorem ipsum</h3>
-					<button class="boton">Saber Más</button>
-				</div>
-			</div>
-			<div class="cardd" style="background-image: url('../img/card_prueba.jpg')">
-				<div class="textos">
-					<h3>Lorem ipsum</h3>
-					<button class="boton">Saber Más</button>
-				</div>
-			</div>
-		</div>
-
-		<div class="banner" style="background-image: url('../img/card_prueba.jpg')">
-			<h3>Lorem ipsum</h3>
-			<ul>
-				<li>Lorem ipsum</li>
-				<li>Lorem ipsum</li>
-				<li>Lorem ipsum</li>
-				<li>Lorem ipsum</li>
-				<li>Lorem ipsum</li>
-				<li>Lorem ipsum</li>
-			</ul>
-			<button class="boton">Saber Más</button>
-		</div>
-	</div>
-    <!-- Card Banner 1-->
-    <div class="cards-banner-one">
-      <div class="content">
-        <h2>Lorem ipsum</h2>
-        <!-- lorem 20 -->
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, amet dolorem! 
-            Vitae amet, accusamus neque reprehenderit quidem commodi magni? Obcaecati!</p>
-            <input type="button" value="Leer Más">
-      </div>
-    </div>
-    <div class="cards-banner-one">
-      <div class="content">
-        <h2>Lorem ipsum</h2>
-        <!-- lorem 20 -->
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, amet dolorem! 
-            Vitae amet, accusamus neque reprehenderit quidem commodi magni? Obcaecati!</p>
-            <input type="button" value="Leer Más">
-      </div>
-    </div>
-
-    <!-- Cards -->
-    <div class="Body_de_la_pagina">
-        <div class="container_card">
-                <div class="card c1">
-                    <div class="icon">
-                    <i class="fa-solid fa-people-group"></i>
-                    </div>
-                    <div class="descripcion">
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae repellendus officia eius iure, id explicabo 
-                            perspiciatis fugit neque fuga a distinctio nobis nemo consectetur, ad tempore nihil tempora modi molestias minus 
-                            nulla voluptas veritatis minima temporibus? Fugit cumque, error voluptas delectus consequatur nostrum tempore, explicabo 
-                            omnis ducimus veniam perferendis repellendus.</p>
-                            <input type="button" value="Leer Más">
-                    </div>
+    <!-- Pie de Pagina -->
+    <footer>
+        <div class="container__footer">
+            <div class="box__footer">
+                <div class="logo">
+                    <img src="../img/logo.png" alt="">
                 </div>
-                <div class="card c2">
-                    <div class="icon">
-                    <i class="fa-solid fa-wallet"></i>
-                    </div>
-                    <div class="descripcion">
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae repellendus officia eius iure, id explicabo 
-                            perspiciatis fugit neque fuga a distinctio nobis nemo consectetur, ad tempore nihil tempora modi molestias minus 
-                            nulla voluptas veritatis minima temporibus? Fugit cumque, error voluptas delectus consequatur nostrum tempore, explicabo 
-                            omnis ducimus veniam perferendis repellendus.</p>
-                            <input type="button" value="Leer Más">
-                    </div>
-                </div>
-                <div class="card c3">
-                    <div class="icon">
-                    <i class="fa-solid fa-tags"></i>
-                    </div>
-                    <div class="descripcion">
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae repellendus officia eius iure, id explicabo 
-                            perspiciatis fugit neque fuga a distinctio nobis nemo consectetur, ad tempore nihil tempora modi molestias minus 
-                            nulla voluptas veritatis minima temporibus? Fugit cumque, error voluptas delectus consequatur nostrum tempore, explicabo 
-                            omnis ducimus veniam perferendis repellendus.</p>
-                            <input type="button" value="Leer Más">
-                    </div>
-                </div>
-        </div>
-    </div>
-
-    <!--Pie de Pagina-->
-    <footer class="pie-pagina">
-        <div class="grupo-1">
-            <div class="box">
-                <figure>
-                    <a href="#">
-                        <img src="../img/logo.png" alt="Logo" style="width:120px;">
-                    </a>
-                </figure>
-            </div>
-            <div class="box">
-                <h2>SOBRE NOSOTROS</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
-            </div>
-            <div class="box">
-                <h2>SIGUENOS</h2>
-                <div class="red-social">
-                    <a href="#" class="fa fa-facebook"></a>
-                    <a href="#" class="fa fa-instagram"></a>
-                    <a href="#" class="fa fa-twitter"></a>
-                    <a href="#" class="fa fa-youtube"></a>
+                <div class="terms">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde perferendis veniam maiores, voluptatem molestiae repellendus 
+                    sunt consequuntur iste laboriosam iusto recusandae, doloribus ipsa, cumque natus placeat provident similique. Cum, itaque?</p>
                 </div>
             </div>
+            <div class="box__footer">
+                <h2>Soluciones</h2>
+                <a href="#">Prestamos</a>
+                <a href="#">Seguro de Vida</a>
+                <a href="#">Inversiones</a>
+                <a href="#">Credito</a>
+            </div>
+            <div class="box__footer">
+                <h2>Compañia</h2>
+                <a href="#">Acerca de </a>
+                <a href="#">Trabajos</a>
+                <a href="#">Repositorio</a>
+                <a href="#">Ayuda</a>
+            </div>
+            <div class="box__footer">
+                <h2>Redes Sociales</h2>
+                <a href="#"> <i class="fab fa-facebook-square"></i> Facebook</a>
+                <a href="#"><i class="fab fa-twitter-square"></i> Twitter</a>
+                <a href="#"><i class="fab fa-linkedin"></i> Linkedin</a>
+                <a href="#"><i class="fab fa-instagram-square"></i> Instagram</a>
+            </div>
         </div>
-        <div class="grupo-2">
-            <small>&copy; 2022 <b>SUS Bank</b> - Todos los Derechos Reservados.</small>
+        <div class="box__copyright">
+            <hr>
+            <p>Todos los derechos reservados © 2022 <b>SUS BANK</b></p>
         </div>
     </footer>
+    <!-- Fin Pie de Pagina -->
+    </div>
+   <script src="../configuracion/js/app.js"></script>
 </body>
 </html>
