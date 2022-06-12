45440330141738c26a739f69c43ea9cd1260b8c9
Miguel-Rayon <miguel.hdz.rayon@gmail.com>
2022-06-12 12:35:17 -0500

    version 0.1.4

--- configuracion/js/app_login.js
+++ configuracion/js/app_login.js
@@ -1,46 +1,11 @@
-/**
- * Animacion del scroll para que se vea chingon xd
- */
- window.onscroll = function(){
-    scroll = document.documentElement.scrollTop;
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-    header = document.getElementById("header");
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
 
-    if (scroll > 20){
-        header.classList.add('nav_mod');
-    }else if(scroll < 20){
-        header.classList.remove('nav_mod');
-    }
-}
-/**
- * Fin de la animacion del scroll
- */
-/**
- * Mover la barra de navegacion
- */
-document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
-
-menu = document.getElementById("header");
-body = document.getElementById("container_all");
-nav = document.getElementById("nav");
-
-    function mostrar_menu(){
-        
-        menu.classList.toggle('move_content');
-        body.classList.toggle('move_content');
-        nav.classList.toggle('move_nav');
-    }
-
-
-window.addEventListener("resize", function(){
-
-    if(window.innerWidth > 760){
-    menu.classList.remove('move_content');
-    body.classList.remove('move_content');
-    nav.classList.remove('move_nav');
-    }
-
-})
-/**
- * Fin de mover la barra de navegacion 
- */
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
