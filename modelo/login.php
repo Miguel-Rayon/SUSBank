47b6d1e1a924c15820e765a096c4f1f3b08e5d63
Miguel-Rayon <miguel.hdz.rayon@gmail.com>
2022-06-24 13:56:02 -0500

    version 0.2.1b

--- modelo/login.php
+++ modelo/login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 include 'conexion.php';
@@ -23,30 +22,4 @@ class login{
      }
 }
 
-=======
-<?php
-
-include 'conexion.php';
-
-class login{
-
-    private $db; //database
-    private $lista;
-
-    public function __construct(){
-        $this->db = conexion::conn();
-        $this->arraydb = array();
-     }
-
-
-     public function inicio_sesion($correo,$pass){
-         $resultado = $this->db->query("SELECT * FROM usuarios WHERE correo='$correo' AND pass='$pass'");
-         while($filas = $resultado->fetch_assoc()){
-                $this->lista[] = $filas;
-         }
-         return $this->lista;
-     }
-}
-
->>>>>>> 808b9d5444bc9176c6ff9f99d86211fe06e31d21
 ?>
--- vista/envio_correo/enviar_correo.js
+++ vista/envio_correo/enviar_correo.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function enviar_correo() {
     var correo = document.getElementById('correo').value;
     var nombre = document.getElementById('nombre').value;
@@ -34,41 +33,4 @@ function enviar_correo() {
         }
 
     });
-=======
-function enviar_correo() {
-    var correo = document.getElementById('correo').value;
-    var nombre = document.getElementById('nombre').value;
-    var mensaje = document.getElementById('mensaje').value;
-    $.ajax({
-        url: "enviar_correo.php",
-        type: "POST",
-        data: "correo=" + correo + "&nombre=" + nombre + "&mensaje=" + mensaje,
-        success: function(resp) {
-            var respuesta = JSON.parse(resp);
-            if (respuesta.status == "1") {
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Exito!',
-                    text: 'El correo se ha enviado exitosamente!',
-                    showCancelButton: false,
-                    confirmButtonColor: '#343a40',
-                    confirmButtonText: 'Continuar'
-                    
-                  }).then((result) => {
-                    if (result.isConfirmed) {
-                        window.location.replace(respuesta.url)
-                    }
-                })
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Correo no enviado!'
-                  })
-            }
-
-        }
-
-    });
->>>>>>> 808b9d5444bc9176c6ff9f99d86211fe06e31d21
 }
