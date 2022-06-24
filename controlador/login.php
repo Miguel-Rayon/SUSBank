15e72d5ab0b3edb72ec61065b376579ac8f87d7b
Miguel-Rayon <miguel.hdz.rayon@gmail.com>
2022-06-24 13:55:27 -0500

    version 0.2.1b

--- controlador/login.php
+++ controlador/login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 $correo = $_POST['correo_login'];
@@ -25,32 +24,4 @@ if(empty($resultado)){
         "url" => "$url"
     ]));
 }
-=======
-<?php
-session_start();
-$correo = $_POST['correo_login'];
-$pass = $_POST['pass_login'];
-
-include("../modelo/login.php");
-$obj = new Login();
-$resultado = $obj -> inicio_sesion($correo,$pass);
-
-if(empty($resultado)){
-    exit(json_encode([
-        "status" => "2"
-    ]));
-}else{
-    foreach($resultado as $r){
-        $nombre = $r['nombre'];
-    }
-    $_SESSION['login'] = "ok";
-    $_SESSION['nombre'] =  $nombre;
-    $url = "home.html";
-    exit(json_encode([
-        "status" => "1",
-        "nombre" => $nombre,
-        "url" => "$url"
-    ]));
-}
->>>>>>> 808b9d5444bc9176c6ff9f99d86211fe06e31d21
 ?>
--- controlador/registro.php
+++ controlador/registro.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 include ("gen_card_y_sald.php"); 
@@ -26,33 +25,4 @@ exit(json_encode([
 ]));
 
 
-=======
-<?php
-session_start();
-include ("gen_card_y_sald.php"); 
-$nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$pass = $_POST['pass'];
-$curp = $_POST['curp'];
-$cel = $_POST['cel'];
-$num_tarjeta=$card;
-$saldo= $sald;
-$clabe=$c;
-
-
-include("../modelo/registro.php");
-$obj = new agregar();
-$resultado = $obj -> agregar_usuario($nombre,$correo,$pass,$curp,$cel,$num_tarjeta,$saldo,$clabe);
-
-
-$_SESSION['nombre'] =  $nombre;
-$url = "home.html";
-exit(json_encode([
-    "status" => "1",
-    "nombre" => $nombre,
-    "url" => "$url"
-]));
-
-
->>>>>>> 808b9d5444bc9176c6ff9f99d86211fe06e31d21
 ?>
