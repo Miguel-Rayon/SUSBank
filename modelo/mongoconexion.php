a2b194ac54701173352d9e74e387f0cc1950861f
Miguel-Rayon <miguel.hdz.rayon@gmail.com>
2022-09-27 08:02:40 -0500

    cc

--- modelo/mongoconexion.php
+++ modelo/mongoconexion.php
@@ -1,7 +1,5 @@
 <?php
-
-    require_once "../vendor/autoload.php";
-
+    require_once $_SERVER['DOCUMENT_ROOT']. "/SUSBank/vendor/autoload.php";
     class Conexion{
         public function conectar(){
             try {
